315b23966ddf6c8c2ffc1dcdfbc6775e179a9102
Xuanwo <xuanwo.cn@gmail.com>
2016-02-20 14:58:32 +0800

    update datagrip to 1.0.2

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName= 'datagrip'
+$installerType = 'EXE'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.jetbrains.com/datagrip/datagrip-1.0.2.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
