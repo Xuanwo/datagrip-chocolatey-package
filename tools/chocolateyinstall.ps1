deab574773986c5ed92efa49db6b987c50adc5eb
Xuanwo <xuanwo.cn@gmail.com>
2016-11-06 00:01:30 +0800

    Update install script to make ci happy

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,14 @@
 ﻿$packageName= 'datagrip'
 $installerType = 'EXE'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.jetbrains.com/datagrip/datagrip-2016.2.5.exe'
+$version    = '2016.2.5'
+$url        = "https://download.jetbrains.com/datagrip/datagrip-$version.exe"
 $silentArgs = '/S'
 $validExitCodes = @(0)
 $checksum   = '6f8fa8477010d3d49e61b10844876db2df1ec0f852fdaf7cbd9aec167e37214d'
-$checksumtype = 'sha256'
+$checksumType = 'sha256'
+$fileFullPath = "$toolsDir\datagrip-$version.exe"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$checksum" "$checksumtype" -validExitCodes $validExitCodes
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$fileFullPath" -Url "$url"
+Get-ChecksumValid -File "$fileFullPath" -CheckSum "$checksum" -ChecksumType "$checksumType"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
