5252bb11dbe5147a6eed5a02a4f373af9661cacf
Zed <dev@zedcn.vom>
2017-04-10 12:42:02 +0800

    update to 2017.1.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName= 'datagrip'
 $installerType = 'EXE'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$version    = '2017.1'
+$version    = '2017.1.1'
 $url        = "https://download.jetbrains.com/datagrip/datagrip-$version.exe"
 $silentArgs = '/S'
 $validExitCodes = @(0)
-$checksum   = '7360ce2e21a9f8602c9cc5edc281d8ed773cbb05b23ef4bcc1ba930e98b24f23'
+$checksum   = '6aa8f4b0803b67b9a9c4616fe15da8ee51f9488569d98162a41e52e1a5ddb92b'
 $checksumType = 'sha256'
 $fileFullPath = "$toolsDir\datagrip-$version.exe"
 
